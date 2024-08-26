26247bdc53d36f2e68d5c0dab4ede334b68496e3
daniescobarx <daniela.mollo@sptech.school>
2024-08-26 19:09:44 -0300

    Initial commit

---
+++ src/main/java/com/application/petcare/PetCareRestApplication.java
@@ -0,0 +1,13 @@
+package com.application.petcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCareRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/petcare/PetCareRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
