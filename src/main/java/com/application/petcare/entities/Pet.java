296e350e8df6e312874153b19535115b310fe80e
daniescobarx <daniela.mollo@sptech.school>
2024-08-30 20:58:32 -0300

    refactor: att entity pet

--- src/main/java/com/application/petcare/entities/Pet.java
+++ src/main/java/com/application/petcare/entities/Pet.java
@@ -1,10 +1,13 @@
 package com.application.petcare.entities;
 
+import com.application.petcare.enums.Tamanho;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -22,10 +25,32 @@ public class Pet {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
-    private String species;
-
     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
+
+    @Column(nullable = false)
+    private String species;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private double weight;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Tamanho size;
+
+    @Column(nullable = true)
+    private String microchip;
+
+    @Column(name = "notes")
+    private String notes;
 }
