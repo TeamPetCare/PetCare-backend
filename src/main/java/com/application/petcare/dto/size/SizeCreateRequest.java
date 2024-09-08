6db87dc0fcf310883df656f08e5370e08d8d9a7f
daniescobarx <daniela.mollo@sptech.school>
2024-09-08 15:29:14 -0300

    feat: add size dtos

---
+++ src/main/java/com/application/petcare/dto/size/SizeCreateRequest.java
@@ -0,0 +1,17 @@
+package com.application.petcare.dto.size;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class SizeCreateRequest {
+
+    @NotBlank(message = "Tipo Tamanho é obrigatório")
+    private String tipoTamanho;
+
+    @NotNull(message = "Preco é obrigatório")
+    private BigDecimal preco;
+}
