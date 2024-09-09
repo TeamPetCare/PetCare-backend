0f0c8c21c182630529b938c31d3e35de5553c20b
daniescobarx <daniela.mollo@sptech.school>
2024-09-09 20:41:41 -0300

    feat: add config swagger Employee

--- src/main/java/com/application/petcare/controller/EmployeeController.java
+++ src/main/java/com/application/petcare/controller/EmployeeController.java
@@ -11,32 +11,39 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
+@Tag(name = "Employee", description = "Endpoints para gerenciar funcionários")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @Operation(summary = "Criar um novo funcionário")
     @PostMapping
     public ResponseEntity<EmployeeResponse> createEmployee(@RequestBody EmployeeCreateRequest employeeCreateRequest) {
         EmployeeResponse createdEmployee = employeeService.createEmployee(employeeCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployee);
     }
 
+    @Operation(summary = "Buscar funcionário por ID")
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable UUID id) {
         EmployeeResponse employeeResponse = employeeService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(employeeResponse);
     }
 
+    @Operation(summary = "Buscar todos os funcionários")
     @GetMapping
     public ResponseEntity<List<EmployeeResponse>> getAllEmployees() {
         List<EmployeeResponse> employees = employeeService.findAllEmployees();
         return ResponseEntity.status(HttpStatus.OK).body(employees);
     }
 
+    @Operation(summary = "Atualizar funcionário por ID")
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeResponse> updateEmployee(
             @PathVariable UUID id,
@@ -45,6 +52,7 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedEmployee);
     }
 
+    @Operation(summary = "Excluir funcionário por ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable UUID id) {
         employeeService.deleteEmployee(id);
