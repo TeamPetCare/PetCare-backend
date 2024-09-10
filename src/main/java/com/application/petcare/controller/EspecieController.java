906a917380fa5217d85a1a62b18bbbcf72f509d7
daniescobarx <daniela.mollo@sptech.school>
2024-09-10 11:43:10 -0300

    feat: add swagger in EspecieController

--- src/main/java/com/application/petcare/controller/EspecieController.java
+++ src/main/java/com/application/petcare/controller/EspecieController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/api/especies")
@@ -19,32 +22,39 @@ public class EspecieController {
 
     private final EspecieService especieService;
 
+    @Operation(summary = "Criar uma nova espécie", description = "Cria uma nova espécie com os dados fornecidos.")
     @PostMapping
-    public ResponseEntity<EspecieResponse> createEspecie( @Valid
-            @RequestBody EspecieCreateRequest request){
+    public ResponseEntity<EspecieResponse> createEspecie(@Valid @RequestBody EspecieCreateRequest request) {
         return ResponseEntity.ok(especieService.createEspecie(request));
     }
 
+    @Operation(summary = "Obter uma espécie pelo ID", description = "Retorna os detalhes de uma espécie específica pelo ID.")
     @GetMapping("/{id}")
-    public ResponseEntity<EspecieResponse> getEspecieById(@PathVariable UUID id){
+    public ResponseEntity<EspecieResponse> getEspecieById(@PathVariable UUID id) {
         return ResponseEntity.ok(especieService.getEspecieById(id));
     }
 
+    @Operation(summary = "Listar todas as espécies", description = "Retorna uma lista de todas as espécies cadastradas.")
     @GetMapping
-    public ResponseEntity<List<EspecieResponse>> getAllEspecies(){
+    public ResponseEntity<List<EspecieResponse>> getAllEspecies() {
         return ResponseEntity.ok(especieService.getAllEspecies());
     }
 
+    @Operation(summary = "Atualizar uma espécie existente", description = "Atualiza os dados de uma espécie existente pelo ID.")
     @PutMapping("/{id}")
-    public ResponseEntity<EspecieResponse> updateEspecie(@PathVariable UUID id, @Valid
-    @RequestBody EspecieCreateRequest request){
+    public ResponseEntity<EspecieResponse> updateEspecie(
+            @PathVariable UUID id,
+            @Valid @RequestBody EspecieCreateRequest request) {
         return ResponseEntity.ok(especieService.updateEspecie(id, request));
     }
 
+    @Operation(summary = "Deletar uma espécie pelo ID", description = "Deleta uma espécie existente pelo ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No Content, quando a espécie é deletada com sucesso")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEspecie(@PathVariable UUID id){
+    public ResponseEntity<Void> deleteEspecie(@PathVariable UUID id) {
         especieService.deleteEspecie(id);
         return ResponseEntity.noContent().build();
     }
-
 }
