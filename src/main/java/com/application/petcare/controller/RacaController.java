2f6f1b55399a2bdb725e116d360c0ff9ea011acf
daniescobarx <daniela.mollo@sptech.school>
2024-09-10 11:56:06 -0300

    feat: add swagger in RacaController

--- src/main/java/com/application/petcare/controller/RacaController.java
+++ src/main/java/com/application/petcare/controller/RacaController.java
@@ -7,6 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,26 +21,36 @@ public class RacaController {
 
     private final RacaService racaService;
 
+    @Operation(summary = "Criar uma nova raça", description = "Cria uma nova raça com os dados fornecidos.")
     @PostMapping
     public ResponseEntity<RacaResponse> createRaca(@Valid @RequestBody RacaCreateRequest request) {
         return ResponseEntity.ok(racaService.createRaca(request));
     }
 
+    @Operation(summary = "Atualizar uma raça existente", description = "Atualiza os dados de uma raça existente pelo ID.")
     @PutMapping("/{id}")
-    public ResponseEntity<RacaResponse> updateRaca(@PathVariable UUID id, @Valid @RequestBody RacaCreateRequest request) {
+    public ResponseEntity<RacaResponse> updateRaca(
+            @PathVariable UUID id,
+            @Valid @RequestBody RacaCreateRequest request) {
         return ResponseEntity.ok(racaService.updateRaca(id, request));
     }
 
+    @Operation(summary = "Obter uma raça pelo ID", description = "Retorna os detalhes de uma raça específica pelo ID.")
     @GetMapping("/{id}")
     public ResponseEntity<RacaResponse> getRacaById(@PathVariable UUID id) {
         return ResponseEntity.ok(racaService.getRacaById(id));
     }
 
+    @Operation(summary = "Listar todas as raças", description = "Retorna uma lista de todas as raças cadastradas.")
     @GetMapping
     public ResponseEntity<List<RacaResponse>> getAllRacas() {
         return ResponseEntity.ok(racaService.getAllRacas());
     }
 
+    @Operation(summary = "Deletar uma raça pelo ID", description = "Deleta uma raça existente pelo ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No Content, quando a raça é deletada com sucesso")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteRaca(@PathVariable UUID id) {
         racaService.deleteRaca(id);
