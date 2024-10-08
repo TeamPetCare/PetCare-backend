a0dda2a313a65577b15a35734b1ad2e3ac80d916
daniescobarx <dani.0paula31@gmail.com>
2024-10-08 20:04:50 -0300

    chore: remove unused classes and clean up code

--- src/main/java/com/application/petcare/repository/CreditoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.application.petcare.repository;
-
-import com.application.petcare.entities.Credito;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditoRepository extends JpaRepository<Credito, Integer> {
-}
--- src/main/java/com/application/petcare/repository/DebitoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.application.petcare.repository;
-
-import com.application.petcare.entities.Debito;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DebitoRepository extends JpaRepository<Debito, Integer> {
-}
