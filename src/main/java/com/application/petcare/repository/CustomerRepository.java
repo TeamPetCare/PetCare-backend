a5f211d77655e9f29133a1f2c1ccf8f19acbf750
daniescobarx <daniela.mollo@sptech.school>
2024-10-16 23:17:13 -0300

    refactor: remove unused classes
    - Removed Employee, Customer, and Owner classes
    - Consolidated functionality into the User class
    - Updated related services and controllers

--- src/main/java/com/application/petcare/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.petcare.repository;
-
-import com.application.petcare.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
--- src/main/java/com/application/petcare/repository/OwnerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.application.petcare.repository;
-
-import com.application.petcare.entities.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface OwnerRepository extends JpaRepository<Owner, Integer> {
-}
